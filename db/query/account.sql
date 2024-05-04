9d4a5132204bc7373b2694cdc915095290ea8297
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-05-04 12:55:57 +0530

    completed CRUD for accounts

--- db/query/account.sql
+++ db/query/account.sql
@@ -7,3 +7,9 @@ Insert Into accounts (owner, balance, currency) values ($1, $2, $3) Returning *;
 
 -- name: ListAccounts :many
 Select * from accounts order by id LIMIT $1 Offset $2;
+
+-- name: UpdateAccount :one
+Update accounts set balance=$2 where id = $1 Returning *;
+
+-- name: DeleteAccount :exec
+Delete from accounts where id = $1;
--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -33,6 +33,15 @@ func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (A
 	return i, err
 }
 
+const deleteAccount = `-- name: DeleteAccount :exec
+Delete from accounts where id = $1
+`
+
+func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteAccount, id)
+	return err
+}
+
 const getAccount = `-- name: GetAccount :one
 SELECT id, owner, balance, currency, created_at, country_code FROM accounts
 WHERE id = $1 LIMIT 1
@@ -89,4 +98,27 @@ func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]A
 		return nil, err
 	}
 	return items, nil
+}
+
+const updateAccount = `-- name: UpdateAccount :one
+Update accounts set balance=$2 where id = $1 Returning id, owner, balance, currency, created_at, country_code
+`
+
+type UpdateAccountParams struct {
+	ID      int64 `json:"id"`
+	Balance int64 `json:"balance"`
+}
+
+func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error) {
+	row := q.db.QueryRowContext(ctx, updateAccount, arg.ID, arg.Balance)
+	var i Account
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Currency,
+		&i.CreatedAt,
+		&i.CountryCode,
+	)
+	return i, err
 }
