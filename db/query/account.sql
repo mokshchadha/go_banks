a18639f5020ddd8352e917a359dac2d2ed11a5e5
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-05-04 12:51:39 +0530

    added more queries for sqlc

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,3 +1,9 @@
 -- name: GetAccount :one
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
+
+-- name: CreateAccount :one
+Insert Into accounts (owner, balance, currency) values ($1, $2, $3) Returning *;
+
+-- name: ListAccounts :many
+Select * from accounts order by id LIMIT $1 Offset $2;
