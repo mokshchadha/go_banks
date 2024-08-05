75625ab820005d6e3216e84c651b4d3cf54d26c6
moksh <chadhamoksh@gmail.com>
2024-08-05 13:23:44 +0530

    custom validators added

--- db/util/random.go
+++ db/util/random.go
@@ -38,7 +38,7 @@ func RandomMoney() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "INR", "CAD"}
+	currencies := []string{CAD, EUR, USD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
