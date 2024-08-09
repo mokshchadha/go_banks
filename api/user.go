1ff3c4bb7bff34be0d55b15f88d23a723efc6637
moksh <chadhamoksh@gmail.com>
2024-08-09 17:41:14 +0530

    closing call

---
+++ api/user.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/mokshchadha/go_banks/db/sqlc"
+	"github.com/mokshchadha/go_banks/db/util"
+)
+
+type createUserRequest struct {
+	UserName string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min-6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.UserName,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+
+}
